cd5f0c3c3e25581245ca5f2bdea49b367229ac09
Black <jens.vercauteren@student.howest.be>
2017-09-28 10:23:49 +0200

    Basic-programming-les-2

--- Git_demo.py
+++ Git_demo.py
@@ -7,3 +7,13 @@ print ("how you doin")
 print ("boi")
 
 #commentaar 
+totaal_seconden = int(input("geef het aantal seconden op: "))
+aantal_dagen = totaal_seconden // 86400
+rest1 = totaal_seconden % (24*60*60)
+aantal_uren = rest1 // 3600
+rest2 =  rest1 % 3600
+aantal_minuten = rest2 // 60
+rest3= rest2 % 60
+aantal_seconden = rest3
+
+print("het aantal dagen bedraagt %s.\n Het aantal uren bedraagt %s.\n Het aantal minuten bedraagt %s.\n Het aantal seconden bedraagt %s" % (aantal_dagen, aantal_uren, aantal_minuten, aantal_seconden))
