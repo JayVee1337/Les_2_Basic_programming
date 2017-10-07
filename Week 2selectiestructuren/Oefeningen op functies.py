9b24fc178366931ccfdebe4bb8d76d984bc6fe5f
Black <jens.vercauteren@student.howest.be>
2017-10-07 18:29:42 +0200

    Basic-programming-les-2

--- Week 2selectiestructuren/Oefeningen op functies.py
+++ Week 2selectiestructuren/Oefeningen op functies.py
@@ -30,33 +30,55 @@
 # printMaximum (6532, 312, 643516543)
 
 #Oefening 5: Maandnummer
-maand = float(input("geef een maandnummer op "))
-def maandNummer (maand):
-    if (maand == 1):
-        print ("Dit is de maand januari")
-    elif (maand == 2):
-        print ("Dit is de maand februari")
-    elif (maand == 3):
-        print ("Dit is de maand maart")
-    elif (maand == 4):
-        print ("Dit is de maand april")
-    elif (maand == 5):
-        print ("Dit is de maand mei")
-    elif (maand == 6):
-        print ("Dit is de maand juni")
-    elif (maand == 7):
-        print("Dit is de maand juli")
-    elif (maand == 8):
-        print("Dit is de maand augustus")
-    elif (maand == 9):
-        print("Dit is de maand september")
-    elif (maand == 10):
-        print("Dit is de maand oktober")
-    elif (maand == 11):
-        print("Dit is de maand november")
-    elif (maand == 12):
-        print("Dit is de maand december")
-    elif (maand >= 13):
-        print ("Dit is niet mogelijk!!")
+# maand = float(input("geef een maandnummer op "))
+# def maandNummer (maand):
+#     if (maand == 1):
+#         print ("Dit is de maand januari")
+#     elif (maand == 2):
+#         print ("Dit is de maand februari")
+#     elif (maand == 3):
+#         print ("Dit is de maand maart")
+#     elif (maand == 4):
+#         print ("Dit is de maand april")
+#     elif (maand == 5):
+#         print ("Dit is de maand mei")
+#     elif (maand == 6):
+#         print ("Dit is de maand juni")
+#     elif (maand == 7):
+#         print("Dit is de maand juli")
+#     elif (maand == 8):
+#         print("Dit is de maand augustus")
+#     elif (maand == 9):
+#         print("Dit is de maand september")
+#     elif (maand == 10):
+#         print("Dit is de maand oktober")
+#     elif (maand == 11):
+#         print("Dit is de maand november")
+#     elif (maand == 12):
+#         print("Dit is de maand december")
+#     elif (maand >= 13):
+#         print ("Dit is niet mogelijk!!")
+#
+# maandNummer(maand)
 
-maandNummer(maand)
+# Oefening 6: Celsius en Fahrenheit
+Eenheid_Temperatuur= int(input("Welke eenheid gebruikt u? (1 = Celsius/2 = Fahrenheit)"))
+temp_in_celsius= int
+temp_in_fahrenheit= int
+
+if (Eenheid_Temperatuur == 1):
+    temp_in_celsius = (int(input("geef een temperatuur in Fahrenheit")))
+elif (Eenheid_Temperatuur == 2):
+    temp_in_fahrenheit = (int(input("geef een termperatuur in Celsius")))
+
+def geef_celsius( celsius ):
+    celsius = (temp_in_celsius - 32) * 5 / 9
+    print ("dit komt overeen met {0} graden in celsius".format(celsius))
+def geef_fahrenheit( fahrenheit ):
+    fahrenheit = (temp_in_fahrenheit* 9 / 5) + 32
+    print ('dit komt overeen met {0} graden Fahrenheit'.format(fahrenheit))
+if (Eenheid_Temperatuur == 1):
+    (geef_celsius(temp_in_celsius))
+elif (Eenheid_Temperatuur == 2):
+      (geef_fahrenheit(temp_in_fahrenheit))
+else: (print("Gelieve opnieuw te proberen"))
