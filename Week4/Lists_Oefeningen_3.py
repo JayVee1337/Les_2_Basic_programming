4e7a07e8fa0d6b5916a829848fbf5571dd6dca4b
Black <jens.vercauteren@student.howest.be>
2017-10-19 09:55:37 +0200

    Basic-programming-les-2

--- Week4/Lists_Oefeningen_3.py
+++ Week4/Lists_Oefeningen_3.py
@@ -11,8 +11,28 @@ Nieuwe_list_getallen = []
 def vul_lijst_op(lijst):
     for getal in range(1,485,23):
         lijst.append(getal)
+    #pop met index verwijdert dat specifieke element, gewoon pop() verwijdert het laatste element
+    lijst.pop()
+    print(lijst)
+
 def print_lijst(lijst):
     for item in lijst:
         print(item)
+
+def print_lijst_omgekeerd(lijst):
+    aantal = len(lijst)
+    for item in range(aantal-1, -1, -1):
+        print(lijst[item])
+
+
+
+
+
+# def print_omgekeerd(lijst):
+#     #Draait lijst om, en dat is niet de opgave
+#     for i in reversed(Nieuwe_list_getallen):
+#         print(i)
+# vul_lijst_op(Nieuwe_list_getallen)
+# print_lijst(Nieuwe_list_getallen)
+# print_lijst_omgekeerd(Nieuwe_list_getallen)
 vul_lijst_op(Nieuwe_list_getallen)
-print_lijst(Nieuwe_list_getallen)
