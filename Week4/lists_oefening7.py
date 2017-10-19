6361b635fdc887027b5a3fe0ee665cb16807d33d
Black <jens.vercauteren@student.howest.be>
2017-10-19 10:29:01 +0200

    Basic-programming-les-2

---
+++ Week4/lists_oefening7.py
@@ -0,0 +1,8 @@
+# Schrijf een functie ‘max_num_in_list’ met als parameter een list van getallen die uit de list
+# het maximum opzoekt en teruggeeft.
+Getallen = list(range(1, 15))
+
+def max_num_in_list(lijst):
+    print (max(lijst))
+
+max_num_in_list(Getallen)
