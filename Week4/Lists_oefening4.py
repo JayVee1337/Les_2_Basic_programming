ac833208a7ded1e64959135439202f193dfd6d88
Black <jens.vercauteren@student.howest.be>
2017-10-19 10:01:44 +0200

    Basic-programming-les-2

---
+++ Week4/Lists_oefening4.py
@@ -0,0 +1,12 @@
+# Test met eenvoudige voorbeelden wat deze operatoren voor de list betekenen:
+#  Plus-operator tussen 2 lists
+#  Vermenigvuldigingsoperator tussen een list en een natuurlijk getal
+lijst1= [2,5,4,6]
+lijst2= [5,4,6,8,3]
+
+print(lijst1 + lijst2)
+
+print(lijst1*5)
+
+#Plusoperator zet beide lijsten achter elkaar
+#Vermenigvuldigingsoperator maakt x keer de lijst (1,2,3 * 2 wordt 1,2,3,1,2,3)
